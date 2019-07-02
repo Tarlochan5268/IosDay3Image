f2b3c6bc73142859a0ad58907bc3718bf0f47700
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-02 19:38:42 -0400

    first commit

--- IosDay3Image/ViewController.swift
+++ IosDay3Image/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBAction func swtchactionImage(_ sender: Any)
+    {
+        if(swtchImage.isOn)
+        {
+            imgJungle.image = UIImage(named: "gorilla.png")
+        }
+        else
+        {
+            imgJungle.image = UIImage(named: "elephant.png")
+        }
+    }
+    @IBOutlet weak var swtchImage: UISwitch!
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var lblsliderText: UILabel!
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        
+        lblsliderText.text = "\(Int(sender.value))"
+    }
+    @IBAction func actionsetImage(_ sender: Any) {
+        imgJungle.image = UIImage(named: "elephant.png")
+        
+        view.backgroundColor = UIColor.black
+    }
+    @IBOutlet weak var btnSetImage: UIButton!
+    @IBOutlet weak var imgJungle: UIImageView!
+    override func
+        
+        viewDidLoad() {
         super.viewDidLoad()
+        lblsliderText.text = String(Int(slider1.value))
+        lblStper.text = String(stpr1.value)
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var stpr1: UIStepper!
+    
+    @IBOutlet weak var lblStper: UILabel!
+    @IBAction func stpaction(_ sender: UIStepper) {
+        lblStper.text = String(sender.value)
+        if(stpr1.value == 2.0)
+        {
+            imgJungle.image = UIImage(named: "elephant.png")
+        }
+        else if(stpr1.value == 3.0)
+        {
+            imgJungle.image = UIImage(named: "home.png")
+        }
+        else if(stpr1.value == 4.0)
+        {
+            imgJungle.image = UIImage(named: "ice cream.png")
+        }
+        else if(stpr1.value == 5.0)
+        {
+            imgJungle.image = UIImage(named: "lion.png")
+        }
+        else
+        {
+            imgJungle.image = UIImage(named: "gorilla.png")
+        }
+    }
 }
 
