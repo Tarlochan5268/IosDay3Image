f6b78af6f5d202a87a0a982d1aaeff941fb6c366
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-02 18:48:17 -0400

    Initial Commit

---
+++ IosDay3Image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosDay3Image
+//
+//  Created by admin on 2019-07-02.
+//  Copyright © 2019 Tarlochan5268. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
